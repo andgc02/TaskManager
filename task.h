5e89c7c09aa5a1dba502219ada36f85d1a74d0b2
Andre Cabrera <andregcabrera@gmail.com>
2023-09-28 09:53:35 -0700

    Added detailed comments to code

--- task.h
+++ task.h
@@ -1,15 +1,19 @@
 #pragma once
-
 #include <string>
 
+//Task class is a task with a name and a priority
 class Task {
 public:
+	//Constructor to initialize a task object with a name and priority
 	Task(const std::string& name, int priority);
 
+	//Retrieve task name
 	std::string getName() const;
+
+	//Retrieve task priority
 	int getPriority() const;
 
 private:
-	std::string name;
-	int priority;
+	std::string name; //Name of task
+	int priority; //Priority of task
 };
